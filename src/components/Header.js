24e9f11a8092dea083183be0394accee71829ea6
Xoly <Beff.Henriksson@live.se>
2021-09-01 14:28:10 +0200

    added header content and some style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+
 export default function Header() {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleClass = () => {
+    setOpen(!isOpen);
+  };
   return (
     <div className="header">
-      <p>header</p>
+      <div className="headerLeft">
+        <div
+          className={isOpen ? "btnBurger open" : "btnBurger"}
+          onClick={toggleClass}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="logoAndTitle">
+          <a href="#">
+            <img src="/mycinemalogo128.png" />
+          </a>
+        </div>
+      </div>
+      <div className="headerRight">
+        <div className="btnSearch">
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
+      </div>
     </div>
   );
 }
